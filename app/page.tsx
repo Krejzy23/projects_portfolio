d2e7869de284eff93e2c9e9416a59de1aaf3ec50
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-31 07:54:27 +0200

    UI/UIX change text and array

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Home =() => {
   return (
-    <main className="relative flex flex-col justify-center items-center  overflow-hidden mx-auto px-5 sm:px-10">
+    <main className="relative flex flex-col justify-center items-center overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
--- components/Discover.tsx
+++ components/Discover.tsx
@@ -8,7 +8,7 @@ const Discover = () => {
   return (
     <section id="discover" className="
     ">
-      <div className="max-w-7xl relative mx-auto md:py-40 px-4 w-full  left-0 top-0">
+      <div className="max-w-7xl relative mx-auto md:py-40 px-4 w-full left-0 top-0">
         <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
           Discover <br /> My awesome Website & Applications
         </h1>
